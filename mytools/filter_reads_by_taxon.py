44f3d3fdc61c3baa9c120921377512b0c4e50982
M Durrant <mdurrant@DN52egr2.SUNet>
2016-10-19 16:27:31 -0600

    update

--- mytools/filter_reads_by_taxon.py
+++ mytools/filter_reads_by_taxon.py
@@ -1,6 +1,17 @@
 import argparse
 
 # start here when the script is launched
+
+def get_taxon_nodes(nodes_location):
+    with open(nodes_location) as nodes_in:
+        for line in nodes_in:
+            line = line.strip()
+            print line
+    return "PASS"
+
+def get_taxon_hierarchy(taxon_id):
+    pass
+
 if __name__ == "__main__":
 
     # setup the option parser
@@ -16,5 +27,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     args = vars(args)
+
+    taxon_nodes dict = get_taxon_nodes(taxon_nodes)
 
 

a95f55fc366e2d08ae7a54b331cf7d6ddc409ce7
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-10-19 23:12:09 -0600

    update

--- mytools/filter_reads_by_taxon.py
+++ mytools/filter_reads_by_taxon.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 
 # start here when the script is launched
 
+
 def get_taxon_nodes(nodes_location):
     taxon_nodes_dict = defaultdict(str)
     with open(nodes_location) as nodes_in:
@@ -14,6 +15,7 @@ def get_taxon_nodes(nodes_location):
             taxon_nodes_dict[id] = parent_id
     return taxon_nodes_dict
 
+
 def get_taxon_hierarchy(taxon_id, taxon_nodes_dict):
     hierarchy = [taxon_id]
 
@@ -22,6 +24,7 @@ def get_taxon_hierarchy(taxon_id, taxon_nodes_dict):
         hierarchy.append(taxon_id)
 
     return hierarchy
+
 
 def get_required_reads(reads_to_taxid_location, taxon_id):
     assert type(taxon_id) is list, "taxon_id must be a list"
@@ -38,6 +41,17 @@ def get_required_reads(reads_to_taxid_location, taxon_id):
 
     return matching_reads
 
+
+def print_hierarchy(taxon_hierarchy):
+    assert type(taxon_hierarchy) is list, "the taxon hierarchy must be a list"
+
+    level = 0
+    for taxa in taxon_hierarchy[::-1]:
+        indent = ["\t"]*level
+        indent = indent.join("")
+        print indent+taxa
+        level += 1
+
 if __name__ == "__main__":
 
     # setup the option parser
@@ -47,21 +61,21 @@ if __name__ == "__main__":
     # add universal arguments, arguments to be specified regardless of the type of arguments that follow.
     parser.add_argument('fastq_reads',
                         help='The fastq file containing the reads of interest')
+
     parser.add_argument('read_to_taxid',
                         help='A tab-separated file where the first column is the read title and the second'
                              'column is the assigned taxon id')
-    parser.add_argument('taxon_id',
-                        help='The NCBI Taxon ID of the species of interest')
 
-    parser.add_argument('-par', '--extract_parent_reads', required=False,
-                        help='Specify --parent_read_extract if you would like to filter the reads by every read that'
-                             'is binned into each node in the hierarchy. Follow this flag with the location of the'
-                             'NCBI Taxonomy Database that you would like to use to determine the hierarchy')
+    parser.add_argument('taxon_nodes',
+                        help='Location of the NCBI Taxonomy Database nodes.txt file')
+
+    parser.add_argument('taxon_id', help='The NCBI Taxon ID of the species of interest')
+
     parser.add_argument('-ntaxa', '--number_of_parent_taxa', required=False,
                         help='Specify --parent_read_extract if you would like to filter the reads by every read that'
                              'is binned into each node in the hierarchy. Follow this flag with the location of the'
                              'NCBI Taxonomy Database that you would like to use to determine the hierarchy',
-                        type = int, default=0)
+                        type=int, default=0)
 
     args = parser.parse_args()
     args = vars(args)
@@ -69,27 +83,22 @@ if __name__ == "__main__":
     fastq_reads = args['fastq_reads']
     read_to_taxid = args['read_to_taxid']
     taxon_id = args['taxon_id']
-    taxon_nodes = args['extract_parent_reads']
+    taxon_nodes = args['taxon_nodes']
     ntaxa= args['number_of_parent_taxa']
 
-    taxon_hierarchy = [taxon_id]
+    print("Loading the Taxonomy Database...")
+    taxon_nodes_dict = get_taxon_nodes(taxon_nodes)
 
-    if taxon_nodes is None:
-        selected_reads = get_required_reads(read_to_taxid, taxon_hierarchy)
-        print len(selected_reads)
-    else:
+    print("Getting Taxon Hierarchy...")
+    taxon_hierarchy = get_taxon_hierarchy(taxon_id, taxon_nodes_dict)
 
-        print("Loading the Taxonomy Database...")
-        taxon_nodes_dict = get_taxon_nodes(taxon_nodes)
-        print("Getting Taxon Hierarchy...")
-        taxon_hierarchy = get_taxon_hierarchy(taxon_id, taxon_nodes_dict)
-        print("Here is the taxon id hierarchy:\n\t")
-        print taxon_hierarchy
-        if ntaxa != 0:
-            taxon_hierarchy = taxon_hierarchy[0:ntaxa+1]
-        print("Collecting reads binned to the following taxa:\n\t")
-        print taxon_hierarchy
-        selected_reads = get_required_reads(read_to_taxid, taxon_hierarchy)
-        print len(selected_reads)
+    print("Here is the taxon id hierarchy:")
+    print_hierarchy(taxon_hierarchy)
+    taxon_hierarchy = taxon_hierarchy[0:ntaxa+1]
+
+    print("Collecting reads binned to the following taxa:\n\t")
+    print_hierarchy(taxon_hierarchy)
+    selected_reads = get_required_reads(read_to_taxid, taxon_hierarchy)
+    print len(selected_reads)
 
 

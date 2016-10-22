870d4ce7859fa330be1711bea47ce06cf22ed280
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-10-22 16:31:37 -0600

    update

--- mytools/fastq_stack_to_interleave.py
+++ mytools/fastq_stack_to_interleave.py
@@ -20,10 +20,10 @@ def destack_and_interleave(fastq_file, part_B_line, lines_per_leaf, output_file)
 
                 while True:
                     try:
-                        read1 = [file_in2.readline().strip() for i in range(4)]
+                        read1 = [file_in2.readline().strip() for i in range(lines_per_leaf)]
                         read1[0].split()[0] # read1_test
 
-                        read2 = [file_in1.readline().strip() for i in range(4)]
+                        read2 = [file_in1.readline().strip() for i in range(lines_per_leaf)]
                         read2[0].split()[0] # read2_test
 
                         out_file.write("\n".join(read1+read2)+"\n")

9b84a72637bf9a3abd501d1a222871abac8049f1
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-10-22 16:12:51 -0600

    update

--- mytools/fastq_stack_to_interleave.py
+++ mytools/fastq_stack_to_interleave.py
@@ -17,13 +17,15 @@ def destack_and_interleave(fastq_file, part_B_line):
                 line_number += 1
 
             with open(".".join(fastq_file.split(".")[:-1]+["INTERLEAVED",fastq_file.split(".")[-1]]), 'w') as out_file:
-                read1 = ["GO"]
+
                 while True:
                     try:
                         read1 = [file_in2.readline().strip() for i in range(4)]
-                        read2 = [file_in1.readline().strip() for i in range(4)]
+                        read1_test = read1[0].split()[0]
 
-                        test = read1[0].split()[0]
+                        read2 = [file_in1.readline().strip() for i in range(4)]
+                        read2_test = read2[0].split()[0]
+
 
                         print("\n".join(read1))
                         print("\n".join(read2))

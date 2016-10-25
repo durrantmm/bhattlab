59d522e611e0b99ad3387cddbec6c367de2fa972
M Durrant <mdurrant@DN51slgk.SUNet>
2016-10-24 20:40:55 -0600

    update

--- mytools/phylophilter/shared.py
+++ mytools/phylophilter/shared.py
@@ -2,8 +2,9 @@
 def get_taxon_nodes(nodes_locations, logger=None):
     assert type(nodes_locations) is list, "The nodes location must be a list of file locations."
 
-    if logger: logger.info("Loading the taxonomy nodes for the analysis.")
+    if logger: logger.error("Loading the taxonomy nodes for the analysis.")
 
+    logger.error("Loading the taxonomy nodes for the analysis. TWO")
     taxon_nodes_dict = {}
     for location in nodes_locations:
         with open(location) as nodes_in:

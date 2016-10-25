3e2b673392c3186221e7b84acafcccedd3d717e1
M Durrant <mdurrant@DN51oo3p.SUNet>
2016-10-25 00:55:47 -0600

    update

--- mytools/phylophilter/shared_objects.py
+++ mytools/phylophilter/shared_objects.py
@@ -15,7 +15,7 @@ class pairedEndRead:
                              "end read")
 
     def getReads(self):
-        return self.read1+self.read2
+        return "\n".join(self.read1+self.read2)
 
     def getTitles(self):
         return [self.read1_title, self.read2_title]

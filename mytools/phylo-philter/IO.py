3a82fce73e8c629ea26813a44689fbf778c50e9f
M Durrant <mdurrant@DN52egap.SUNet>
2016-10-24 18:21:50 -0600

    update

--- mytools/phylo-philter/IO.py
+++ mytools/phylo-philter/IO.py
@@ -2,8 +2,9 @@ import objects
 
 def read_fastq_paired_ends(fastq_file_object, lines_per_read=4):
 
-    read = objects.pairedEnd([fastq_file_object.readline().strip() for line in range(lines_per_read)],
-                             [fastq_file_object.readline().strip() for line in range(lines_per_read)])
-    yield read
+    while True:
+        read = objects.pairedEnd([fastq_file_object.readline().strip() for line in range(lines_per_read)],
+                                 [fastq_file_object.readline().strip() for line in range(lines_per_read)])
+        yield read
 
 

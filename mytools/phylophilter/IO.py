6ecec31f09fde003545ca68e028e300165e7a0a4
M Durrant <mdurrant@DN51slgk.SUNet>
2016-10-24 20:24:09 -0600

    update

--- mytools/phylophilter/IO.py
+++ mytools/phylophilter/IO.py
@@ -1,11 +1,11 @@
-import objects
+import shared_objects
 
 def read_fastq_paired_ends_interleaved(fastq_file_object, lines_per_read=4):
 
     while True:
         try:
-            read = objects.pairedEnd([fastq_file_object.readline().strip() for line in range(lines_per_read)],
-                                     [fastq_file_object.readline().strip() for line in range(lines_per_read)])
+            read = shared_objects.pairedEnd([fastq_file_object.readline().strip() for line in range(lines_per_read)],
+                                            [fastq_file_object.readline().strip() for line in range(lines_per_read)])
 
             yield read
         except ValueError:
@@ -16,9 +16,14 @@ def reads_to_taxids(reads_to_taxids_file_object, has_header=True):
     if has_header: reads_to_taxids_file_object.readline()
 
     while True:
-        read1 = reads_to_taxids_file_object.readline().strip()
-        read2 = reads_to_taxids_file_object.readline().strip()
-        if len(read1) == 0 or len(read2) == 0: break
-        yield [read1.split('\t'), read2.split('\t')]
+        try:
+            read_class = shared_objects.pairedEndClassification(
+                reads_to_taxids_file_object.readline().strip(),
+                reads_to_taxids_file_object.readline().strip())
+            yield read_class
+        except ValueError:
+            break
+
+
 
 

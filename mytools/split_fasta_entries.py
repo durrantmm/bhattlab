4ce246fc3947bdb43a6909a431bcec06915e98bd
M Durrant <mdurrant@DN52egaa.SUNet>
2016-10-25 16:28:55 -0600

    update

--- mytools/split_fasta_entries.py
+++ mytools/split_fasta_entries.py
@@ -4,4 +4,7 @@ import sys
 from Bio import SeqIO
 handle = open(sys.argv[1], 'rU')
 for record in SeqIO.parse(handle, "fasta"):
-    print(record.id)
+    with open(record.id+".fasta","w") as outfile:
+        SeqIO.write(record, outfile, "fasta")
+
+handle.close()

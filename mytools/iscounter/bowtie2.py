d72a75617ffb35b35714015f43b4e2429524aff6
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-10-28 00:04:54 -0600

    update

--- mytools/iscounter/bowtie2.py
+++ mytools/iscounter/bowtie2.py
@@ -58,4 +58,5 @@ def align(version, refpath, fastq, output_dir, flags=('--quiet', '--no-unal', '-
 
     # stream output from bowtie2
     bowtie_args = ['bowtie2', '-x', refpath, '-U', fastq, '-S %s/%s.sam' % (output_dir, os.path.basename(refpath))] + list(flags)
-    p2 = subprocess.check_call(bowtie_args, stdout=subprocess.PIPE)
+
+    subprocess.check_call(bowtie_args, stdout=subprocess.PIPE)

7600b830c618154711a8a413e0aeebdf324abc01
M Durrant <mdurrant@DN52emla.SUNet>
2016-10-28 17:00:09 -0600

    update

--- mytools/scrape_isfinder.py
+++ mytools/scrape_isfinder.py
@@ -1,21 +1,51 @@
-
 import urllib, urllib2
 from bs4 import BeautifulSoup
-
-isfinder_url = "https://www-is.biotoul.fr/search.php"
-values = {'tout' : 'bacteroides'}
-
-data = urllib.urlencode(values)
-print data
-req = urllib2.Request(isfinder_url, data)
-response = urllib2.urlopen(req)
-the_page = response.read()
-the_url  = response.geturl()
+import mechanize
+import sys, os
+import argparse
 
 
-print the_url
-soup = BeautifulSoup(the_page, "html.parser")
 
-all_links = soup.find_all("a")
-for link in all_links:
-    print link
+
+def main(args):
+    print submit_search(args['search_query'], args['search_field'], args['isfinder_search_url'])
+
+
+def submit_search(search_query, search_field, isfinder_url):
+    br = mechanize.Browser()
+    br.open(isfinder_url)
+    br.select_form("search")
+    br[search_field] = search_query
+    response = br.submit()
+
+    return response.read()
+
+
+
+
+if __name__ == "__main__":
+
+    current_dir = os.path.dirname(__file__)
+
+    # setup the option parser
+    parser = argparse.ArgumentParser(description='')
+
+    # add universal arguments, arguments to be specified regardless of the type of arguments that follow.
+    parser.add_argument('search_query', help='The ISFinder search query of interest.')
+
+    parser.add_argument('--search_field', required=False, type=str,
+                        default="host",
+                        help='The url of the ISFinder Website')
+
+    parser.add_argument('--isfinder_search_url', required=False, type=str,
+                        default="https://www-is.biotoul.fr/search.php",
+                        help='The url of the ISFinder Website')
+
+    parser.add_argument('--output_dir', required=False, type=str,
+                        default= os.path.join(current_dir, "output"),
+                        help='The output directory for the results')
+
+    args = parser.parse_args()
+    args = vars(args)
+
+    main(args)

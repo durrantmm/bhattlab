b4514aee044e15149c9c6bd30cbbfed977ea251a
M Durrant <mdurrant@DN52emla.SUNet>
2016-10-28 15:08:52 -0600

    update

--- mytools/scrape_isfinder.py
+++ mytools/scrape_isfinder.py
@@ -1,7 +1,7 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
+wiki = "https://www-is.biotoul.fr/search.php"
 page = urllib2.urlopen(wiki)
 
 soup = BeautifulSoup(page)

963d8ffc4095cef56c2917f611332ad3d81dc7a5
M Durrant <mdurrant@DN52emla.SUNet>
2016-10-28 15:12:54 -0600

    update

--- mytools/scrape_isfinder.py
+++ mytools/scrape_isfinder.py
@@ -4,6 +4,6 @@ from bs4 import BeautifulSoup
 wiki = "https://www-is.biotoul.fr/search.php"
 page = urllib2.urlopen(wiki)
 
-soup = BeautifulSoup(page)
+soup = BeautifulSoup(page, "html.parser")
 
 print soup.title

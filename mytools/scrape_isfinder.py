99f7d0e4007a488ce77048c67c0810afe467133a
M Durrant <mdurrant@DN52emla.SUNet>
2016-10-28 15:20:43 -0600

    update

--- mytools/scrape_isfinder.py
+++ mytools/scrape_isfinder.py
@@ -1,14 +1,16 @@
-import urllib2
+
+import urllib, urllib2
 from bs4 import BeautifulSoup
 
-wiki = "https://www-is.biotoul.fr/search.php"
-page = urllib2.urlopen(wiki)
+isfinder_url = "https://www-is.biotoul.fr/search.php"
+values = {'tout' : 'bacteroides'}
+
+data = urllib.urlencode(values)
+req = urllib2.Request(isfinder_url, data)
+response = urllib2.urlopen(req)
+the_page = response.read()
+the_url  = response.geturl()
+
+
 
 soup = BeautifulSoup(page, "html.parser")
-
-print soup.title
-
-all_links = soup.find_all("a")
-
-for link in all_links:
-    print link

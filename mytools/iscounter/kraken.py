da118274f20c1a867d6d92134d874ebba62a5d1c
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-11-01 21:57:39 -0600

    update

--- mytools/iscounter/kraken.py
+++ mytools/iscounter/kraken.py
@@ -1,4 +1,4 @@
 import subprocess, sys, os
 
 if __name__ == "__main__":
-    "kraken --db /srv/gsfs0/projects/bhatt/data/program_indices/kraken/kraken_custom"
+    "kraken --db /srv/gsfs0/projects/bhatt/data/program_indices/kraken/kraken_custom --preload "

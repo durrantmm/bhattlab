fec56b48d8222714db60778958f8b2a5d9eef992
M Durrant <mdurrant@DN52eg8j.SUNet>
2016-11-03 16:52:48 -0600

    update

--- mytools/iscounter2/tests/tests.py
+++ mytools/iscounter2/tests/tests.py
@@ -1,11 +1,14 @@
-from mytools.iscounter2.phylophilter import *
+from mytools.iscounter2.phylophilter import IO
+import os, sys
 
 def IO_get_insertion_alignments_TEST(sam_file):
 
-    sam_dict = get_insertion_alignments(sam_file)
+    sam_dict = IO.get_insertion_alignments(sam_file)
 
 
 
 if __name__ == "__main__":
+    current_dir = os.path.dirname(__file__)
+    data_dir = os.path.join(current_dir, "test_data")
 
-    IO_get_insertion_alignments_TEST()
+    IO_get_insertion_alignments_TEST(os.path.join(data_dir, "Bacteroides_all.fasta.sam"))

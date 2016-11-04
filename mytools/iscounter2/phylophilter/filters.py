bf87d72013bc50b901b7274444da1769e10db39e
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-11-04 00:46:59 -0600

    update

--- mytools/iscounter2/phylophilter/filters.py
+++ mytools/iscounter2/phylophilter/filters.py
@@ -42,7 +42,7 @@ class Filter:
 
         saved_taxonomies = {}
         for reads, classes in zip(self.fastq_paired_gen, self.read_to_taxid_paired_gen):
-
+            print total_read_count
             self.aligned_read, self.aligned_IS = IS_align_gen.next()
             read1, read2 = reads.getTitles()
             class1, class2 = classes.getClassifs()

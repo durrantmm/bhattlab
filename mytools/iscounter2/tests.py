8bc69bbadee9963d3e3a945c9a99cb6cea11818a
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-11-04 00:57:24 -0600

    update

--- mytools/iscounter2/tests.py
+++ mytools/iscounter2/tests.py
@@ -11,5 +11,8 @@ if __name__ == "__main__":
     current_dir = os.path.dirname(__file__)
     data_dir = os.path.join(current_dir, "test_data")
 
+    IO_get_insertion_alignments_TEST(open("ex.sam", 'r'))
+    sys.exit()
     IO_get_insertion_alignments_TEST(open(os.path.join(data_dir, "Bacteroides_all.fasta.sam"), 'r'))
+
 

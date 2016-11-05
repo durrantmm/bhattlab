9ed75a3e4879491e45fe0292387c80064d78bd7d
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-11-04 23:18:59 -0600

    update

--- mytools/iscounter2/phylophilter/shared.py
+++ mytools/iscounter2/phylophilter/shared.py
@@ -50,6 +50,6 @@ def loop_counter(loop_count, total_read_count, logger, loop_count_out=1000000):
     loop_count += 1
     if loop_count == loop_count_out:
         logger.info("Total Reads Processed: %d" % total_read_count)
-        loop_counter = 0
+        loop_count = 0
     return loop_count
 

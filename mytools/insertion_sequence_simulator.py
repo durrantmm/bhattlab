bbd4333f4827114f8d71624e25e628c678bf1da1
M Durrant <mdurrant@DN52ec9u.SUNet>
2016-11-08 14:25:06 -0700

    update

---
+++ mytools/insertion_sequence_simulator.py
@@ -0,0 +1,37 @@
+import argparse
+import sys
+from collections import defaultdict
+
+# start here when the script is launched
+
+def main(args):
+
+
+
+def get_n_indices(ref_file):
+    for line in ref_file:
+        if line.startswith(">"):
+            print line
+
+
+
+
+if __name__ == "__main__":
+
+    # setup the option parser
+    parser = argparse.ArgumentParser(description='Quickly get the taxon id for a given')
+    parser.add_argument('-r', '--reference', required=True, help='FILL THIS OUT')
+    parser.add_argument('-i', '--insertion', required=True, help='FILL THIS OUT')
+    parser.add_argument('-n', '--number', required=False, default=1, help='FILL THIS OUT')
+
+    args = parser.parse_args()
+    args = vars(args)
+
+
+    main(args)
+
+
+
+
+
+
--- mytools/scripts/get_taxon_id.py
+++ mytools/scripts/get_taxon_id.py
@@ -4,31 +4,36 @@ from collections import defaultdict
 
 # start here when the script is launched
 
-def main(args):
-
-
-
-def get_n_indices(ref_file):
-    for line in ref_file:
-        if line.startswith(">"):
-            print line
-
-
-
 
 if __name__ == "__main__":
 
     # setup the option parser
     parser = argparse.ArgumentParser(description='Quickly get the taxon id for a given')
-    parser.add_argument('-r', '--reference', required=True, help='FILL THIS OUT')
-    parser.add_argument('-i', '--insertion', required=True, help='FILL THIS OUT')
-    parser.add_argument('-n', '--number', required=False, default=1, help='FILL THIS OUT')
+    parser.add_argument('name', help='FILL THIS OUT')
+    parser.add_argument('-names','--taxon_names_location', required = False,
+                        default="/srv/gsfs0/projects/bhatt/mdurrant/my_code/bhattlab/mytools/iscounter2/data/TaxonomyDatabase/names.dmp",
+                        help='FILL THIS OUT')
+
+
 
     args = parser.parse_args()
     args = vars(args)
 
+    name = args['name'].upper().split()
+    taxon_names_location = args['taxon_names_location']
 
-    main(args)
+
+
+    with open(taxon_names_location) as names_in:
+        for line in names_in:
+            line = [field.strip() for field in line.strip().split("|")]
+
+            for part in name:
+                if part not in line[1].upper():
+                    break
+            else:
+                print("%s\t%s" % (line[0], line[1]))
+
 
 
 

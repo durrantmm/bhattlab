bc18f03b7305b33e8ff4c307e766b996113fd9cd
M Durrant <mdurrant@DN52egb5.SUNet>
2016-11-10 16:44:38 -0700

    update

--- mytools/iscounter2/align_flanking_reads.py
+++ mytools/iscounter2/align_flanking_reads.py
@@ -53,68 +53,68 @@ def filter_flanks_to_fastq(IS_sam, fastq, classifs, taxa, insertion, out_fastq, 
 
     total_read_count = 0
     flanking_reads_count = 0
-    with open(out_fastq.name,'w') as out_fastq:
-        if logger: logger.info("Beginning read filtering...")
-        for reads, classes in izip(fastq, classifs):
-            total_read_count += 1
-            loop_count = shared.loop_counter(loop_count, total_read_count, logger)
 
-            read1, read2 = reads.getTitles()
-            class1, class2 = classes.getClassifs()
-            # print read1[-10:], read2[-10:], self.aligned_read[-10:]
+    if logger: logger.info("Beginning read filtering...")
+    for reads, classes in izip(fastq, classifs):
+        total_read_count += 1
+        loop_count = shared.loop_counter(loop_count, total_read_count, logger)
 
-            # Check the reads and the classifications align
-            if [read1, read2] != classes.getTitles():
-                if logger: logger.error("The reads do not match")
-                raise IndexError("The reads and the classifications need to be in the same order.")
+        read1, read2 = reads.getTitles()
+        class1, class2 = classes.getClassifs()
+        # print read1[-10:], read2[-10:], self.aligned_read[-10:]
 
-            # Discard it if EITHER READ is UNCLASSIFIED
-            if class1 not in taxa and class2 not in taxa:
-                if read1 == aligned_read:
-                    aligned_read, aligned_IS = IS_sam.next()
-                if read2 == aligned_read:
-                    aligned_read, aligned_IS = IS_sam.next()
+        # Check the reads and the classifications align
+        if [read1, read2] != classes.getTitles():
+            if logger: logger.error("The reads do not match")
+            raise IndexError("The reads and the classifications need to be in the same order.")
 
-            # Check that read1 aligns to insertion sequence
-            elif read1 == aligned_read:
-                tmp_aligned_read, tmp_aligned_IS = IS_sam.next()
-
-                # Check that read2 aligns to insertion sequence, send to intra_IS
-                if read2 == tmp_aligned_read:
-                    aligned_read, aligned_IS = IS_sam.next()
-                    continue
-
-                # Otherwise, increment the read2 taxon_IS_count
-                else:
-                    for IS in aligned_IS:
-                        if IS == insertion and class2 in taxa:
-                            logger.info("Flanking read classified as %s: %s" % (class2, read2))
-                            outread = reads.getReads()[1]
-                            outread[0], outread[-1] = ("%s:TAXON-%s" % (outread[0], class2), outread[-1].strip())
-                            print outread
-                            out_fastq.write("\n".join(outread) + "\n")
-                            flanking_reads_count += 1
-
-                    aligned_read, aligned_IS = tmp_aligned_read, tmp_aligned_IS
-
-            # Check that read2 aligns to insertion sequence
-            elif read2 == aligned_read:
-
-                for IS in aligned_IS:
-                    if IS == insertion and class1 in taxa:
-                        logger.info("Flanking read classified as %s: %s" % (class1, read1))
-                        outread = reads.getReads()[0]
-                        outread[0], outread[-1] = ("%s:TAXON-%s" % (outread[0], class1), outread[-1].strip())
-                        print outread
-                        out_fastq.write("\n".join(outread)+"\n")
-                        flanking_reads_count += 1
-
-
+        # Discard it if EITHER READ is UNCLASSIFIED
+        if class1 not in taxa and class2 not in taxa:
+            if read1 == aligned_read:
+                aligned_read, aligned_IS = IS_sam.next()
+            if read2 == aligned_read:
                 aligned_read, aligned_IS = IS_sam.next()
 
-            # If they are the same class, and neither maps to IS.
-            else:
+        # Check that read1 aligns to insertion sequence
+        elif read1 == aligned_read:
+            tmp_aligned_read, tmp_aligned_IS = IS_sam.next()
+
+            # Check that read2 aligns to insertion sequence, send to intra_IS
+            if read2 == tmp_aligned_read:
+                aligned_read, aligned_IS = IS_sam.next()
                 continue
+
+            # Otherwise, increment the read2 taxon_IS_count
+            else:
+                for IS in aligned_IS:
+                    if IS == insertion and class2 in taxa:
+                        logger.info("Flanking read classified as %s: %s" % (class2, read2))
+                        outread = reads.getReads()[1]
+                        outread[0], outread[-1] = ("%s:TAXON-%s" % (outread[0], class2), outread[-1].strip())
+                        print outread
+                        out_fastq.write("\n".join(outread) + "\n")
+                        flanking_reads_count += 1
+
+                aligned_read, aligned_IS = tmp_aligned_read, tmp_aligned_IS
+
+        # Check that read2 aligns to insertion sequence
+        elif read2 == aligned_read:
+
+            for IS in aligned_IS:
+                if IS == insertion and class1 in taxa:
+                    logger.info("Flanking read classified as %s: %s" % (class1, read1))
+                    outread = reads.getReads()[0]
+                    outread[0], outread[-1] = ("%s:TAXON-%s" % (outread[0], class1), outread[-1].strip())
+                    print outread
+                    out_fastq.write("\n".join(outread)+"\n")
+                    flanking_reads_count += 1
+
+
+            aligned_read, aligned_IS = IS_sam.next()
+
+        # If they are the same class, and neither maps to IS.
+        else:
+            continue
 
     logger.info("Total reads counted: %d" % total_read_count)
     logger.info("Total flanking reads meeting criteria: %d" % flanking_reads_count)

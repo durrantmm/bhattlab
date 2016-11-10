c40e485245375f3a65bb3bfd7626b9209b954adb
M Durrant <mdurrant@DN52egb5.SUNet>
2016-11-10 16:47:33 -0700

    update

--- mytools/iscounter2/align_flanking_reads.py
+++ mytools/iscounter2/align_flanking_reads.py
@@ -27,6 +27,7 @@ def main(args):
     with open(args['out_prefix']+'.fq', 'w') as fq_out:
         fq_out.write("TEST1\n")
         fq_out.write("TEST2\n")
+        sys.exit()
         filtered_fastq = filter_flanks_to_fastq(IS_sam, fastq, classifs, args['taxon'], args['insertion_sequence'],
                                                 fq_out, logger)
 

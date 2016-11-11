b09435b7771c508580a433fe3133086600b299bc
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-11-10 23:39:53 -0700

    update

--- mytools/iscounter2/assign_reads_to_peaks.py
+++ mytools/iscounter2/assign_reads_to_peaks.py
@@ -1,27 +1,43 @@
 import argparse, sys, os
 
 def main(args):
-    peaks = get_peaks(args['peaks_path'])
+    peaks, peaks_dict = get_peaks(args['peaks_path'])
     sam = get_sam(args['sam_path'])
 
+
+    for read in sam:
+        pos = sam[3]
+        nearest_peak = get_nearest_peak(pos, peaks)
+        peaks_dict[nearest_peak] += 1
+
+    print peaks_dict
+
+def get_nearest_peak(pos, peaks):
+    nearest_peak = sys.maxint
+    for peak in peaks:
+        if abs(pos-peak[0]) < nearest_peak or abs(pos-peak[1]) < nearest_peak:
+            nearest_peak = "-".join([str(peak[0]), str(peak[1])])
+    return nearest_peak
 def get_sam(sam_path):
     sam = []
     with open(sam_path) as sam_in:
         for line in sam_in:
             line = line.strip().split()
-            print line
+            sam.append(line)
 
     return sam
 
 
 def get_peaks(peaks_path):
     peaks = []
+    peaks_dict = {}
     with open(peaks_path) as peaks_in:
         for line in peaks_in:
             line = line.strip().split()
-            peaks.append((line[0],line[1]))
+            peaks.append((int(line[0]), int(line[1])))
+            peaks_dict["-".join([line[0],line[1]])] = 0
 
-    return peaks
+    return peaks, peaks_dict
 
 
 

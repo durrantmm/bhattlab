227404c0600bb28efc82f082f55c6d7eea9a853b
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-11-16 23:44:14 -0700

    update

--- mytools/ISPeaks/__main__.py
+++ mytools/ISPeaks/__main__.py
@@ -10,10 +10,13 @@ def main(args):
 
 def genome_fasta(s):
     try:
-        genome, taxon= map(int, s.split(','))
+        print s
+        genome, taxon= s.split(',')
+        if not os.path.isfile(genome): raise TypeError("Genome fasta is not a file.")
         return int(taxon), genome
     except:
-        raise argparse.ArgumentTypeError("Genomes must be entered as \"Taxon1,<reference-fasta1> Taxon2,<reference-fasta2>\"")
+        raise argparse.ArgumentTypeError("Genomes must be entered as \"<reference-fasta1>,Taxon1 "
+                                         "<reference-fasta2>,Taxon2...\"")
 
 def genome_list(path):
     try:
@@ -23,12 +26,46 @@ def genome_list(path):
             for line in genomes_in:
                 line = line.strip().split()
                 if len(line) != 2: raise TypeError()
-                taxon, genome = line
-                out_list.append((taxon, genome))
+                genome, taxon = line
+                out_list.append((int(taxon), genome))
         return out_list
     except:
-        raise argparse.ArgumentTypeError('Must be a path to a tab-seperated file that contains the Taxon in the first '
-                                  'column, and the path to the corresponding genome fasta in the second column')
+        raise argparse.ArgumentTypeError('Must be a path to a tab-seperated file that contains'
+                                         'the path to the genome fasta in the first and the corresponding'
+                                          'taxon in the second column.')
+
+def insertion_fasta(path):
+    try:
+        if not os.path.isfile(path): raise TypeError()
+        return path
+    except:
+        raise argparse.ArgumentTypeError('Please give a path to a valid fasta file.')
+
+
+def fastq_file(path):
+    try:
+        if not os.path.isfile(path): raise TypeError()
+        return path
+    except:
+        raise argparse.ArgumentTypeError('Please give paths to valid fastq files.')
+
+def class_file(path):
+    try:
+        if not os.path.isfile(path): raise TypeError()
+        return path
+    except:
+        raise argparse.ArgumentTypeError('Please give paths to valid class files.')
+
+def output_folder(path):
+    try:
+        if os.path.isdir(path):
+            if len(glob(path+'/*')) > 0:
+                raise TypeError()
+        return path
+    except:
+        raise argparse.ArgumentTypeError('Output folder must be empty or non-existent.')
+
+
 
 if __name__ == "__main__":
 
@@ -47,19 +84,19 @@ if __name__ == "__main__":
                                                       'the peak calls.')
     parser_merged.set_defaults(which="merged")
 
-    parser_single.add_argument('-fq', '--fastqs', required=True, nargs = 2,
+    parser_single.add_argument('-fq', '--fastqs', required=True, nargs = 2, type=fastq_file,
                         help='Two fastq files containing the forward and reverse strands, in that order.')
 
-    parser_single.add_argument('-c', '--classifications', required=True, nargs=2,
+    parser_single.add_argument('-c', '--classifications', required=True, nargs=2, type=class_file,
                         help='Two tab-separated files where the first column is the read title and the second'
                              'column is the assigned taxon id. The first classification file corresponds to the'
                              'forward reads, and the second file corresponds to the reverse reads, in the same order'
                              'with no reads excluded.')
 
-    parser_single.add_argument('-o', '--output-folder', required=True,
+    parser_single.add_argument('-o', '--output-folder', required=True, type=output_folder,
                         help='Specify the output folder to create. If already created, it must be empty.')
 
-    parser_single.add_argument('-is', '--insertion-fasta', required=True, type=str,
+    parser_single.add_argument('-is', '--insertion-fasta', required=True, type=insertion_fasta,
                         default=os.path.join(data_dir, "IS_fastas/Bacteroides_all.fasta"),
                         help='A fasta file containing the insertion sequences of interest,'
                              ' concatenated sequentially in any order.')
@@ -68,7 +105,7 @@ if __name__ == "__main__":
     genomes_in.add_argument('-gf', '--reference-genome-fastas',
                              type=genome_fasta, nargs='*',
                              help='All the reference genomes to analyze in fasta format. Input must be '
-                                'in the format \"<reference-fasta1>,Taxon1 <reference-fasta2>,Taxon2\"')
+                                'in the format \"<reference-fasta1>,Taxon1 <reference-fasta2>,Taxon2...\"')
     genomes_in.add_argument('-gl', '--reference-genome-list',
                              type=genome_list, nargs=1,
                              help='A path to a tab-seperated file that contains the Taxon in the first '

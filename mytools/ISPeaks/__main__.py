634699bafe269fb0c262aeed0724677d66bde5dc
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2016-11-16 23:24:56 -0700

    update

--- mytools/ISPeaks/__main__.py
+++ mytools/ISPeaks/__main__.py
@@ -1,78 +1,91 @@
 import argparse, logging
 import sys, os
-import pprint
+from pprint import pprint
 from datetime import datetime
 from glob import glob
 
 def main(args):
     logging.basicConfig(level=logging.DEBUG, format="\n%(levelname)s:\t%(message)s")
     logger = logging.getLogger()
-    output_folder = args['output_folder'] + '_' + os.path.basename(args['fastq_reads']).split(".")[0]
-    args['fastq_reads'] = os.abspath(args['fastq_reads'])
-    if not os.path.isdir(output_folder): os.mkdir(output_folder)
 
-    logger.info("Saving run info to output folder...")
-    write_run_info(args, output_folder)
+def genome_fasta(s):
+    try:
+        genome, taxon= map(int, s.split(','))
+        return int(taxon), genome
+    except:
+        raise argparse.ArgumentTypeError("Genomes must be entered as \"Taxon1,<reference-fasta1> Taxon2,<reference-fasta2>\"")
 
-    logger.info("Building the insertion sequence indices...")
-    bowtie2.build('2.2.9', args['insertion_sequence_fasta'])
-
-    logger.info("Aligning the fastq file to the insertion sequences...")
-    sam_file_loc = bowtie2.align('2.2.9', args['insertion_sequence_fasta'], args['fastq_reads'], output_folder, threads=args['threads'])
-
-    read_filter = filters.Filter(args['fastq_reads'], args['classification_file'], args['taxon_nodes'], logger_in=logger)
-
-    logger.info("Invoking the Jansen Protocol...")
-    taxon_total_count, taxon_IS_count, potential_transfers, intra_IS = read_filter.filter_reads_ISCounter2(sam_file_loc)
-
-    logger.info("Saving results to file... ")
-    out_file = save_summary_stats(args['fastq_reads'], taxon_total_count, taxon_IS_count, potential_transfers,
-                                  intra_IS, output_folder)
-    logger.info("Results saved to %s" % out_file)
-
-    logger.info("Analysis Complete  :)")
-
+def genome_list(path):
+    try:
+        if not os.path.isfile(path): raise TypeError()
+        out_list = []
+        with open(path) as genomes_in:
+            for line in genomes_in:
+                line = line.strip().split()
+                if len(line) != 2: raise TypeError()
+                taxon, genome = line
+                out_list.append((taxon, genome))
+        return out_list
+    except:
+        raise argparse.ArgumentTypeError('Must be a path to a tab-seperated file that contains the Taxon in the first '
+                                  'column, and the path to the corresponding genome fasta in the second column')
 
 if __name__ == "__main__":
 
     current_dir = os.path.dirname(__file__)
     data_dir = os.path.join(current_dir, "data")
-    output_dir = os.path.join(current_dir, "output2")
-    timestamp = ":".join([str(datetime.now().time()), str(datetime.now().date())])
 
     # setup the option parser
     parser = argparse.ArgumentParser(description='')
 
-    # add universal arguments, arguments to be specified regardless of the type of arguments that follow.
-    parser.add_argument('-fq', '--fastq_reads', required=True,
-                        help='This is a fastq file containing PAIRED END reads that are in an interleaved format.')
+    subparsers = parser.add_subparsers(help="The argument specifying the type of analysis: single, "
+                                            "or merged")
 
-    parser.add_argument('-c', '--classification_file', required=True,
-                        help='A tab-separated file where the first column is the read title and the second'
-                             'column is the assigned taxon id')
+    parser_single = subparsers.add_parser('single', help='Run ISPeaks on a single fastq file.')
+    parser_single.set_defaults(which="single")
+    parser_merged = subparsers.add_parser('merged', help='Run ISPeaks on multiple fastq files, merging them to make '
+                                                      'the peak calls.')
+    parser_merged.set_defaults(which="merged")
 
-    parser.add_argument('-nodes', '--taxon_nodes', required=False, type=list,
-                        default=[
-                            os.path.abspath(os.path.join(data_dir, "TaxonomyDatabase/nodes.dmp")),
-                            os.abspath(os.path.join(data_dir, "TaxonomyDatabase/merged.dmp"))],
-                        help='Location of the NCBI Taxonomy Database nodes.txt file', nargs='*')
+    parser_single.add_argument('-fq', '--fastqs', required=True, nargs = 2,
+                        help='Two fastq files containing the forward and reverse strands, in that order.')
 
-    parser.add_argument('-is', '--insertion_sequence_fasta', required=False, type=str,
+    parser_single.add_argument('-c', '--classifications', required=True, nargs=2,
+                        help='Two tab-separated files where the first column is the read title and the second'
+                             'column is the assigned taxon id. The first classification file corresponds to the'
+                             'forward reads, and the second file corresponds to the reverse reads, in the same order'
+                             'with no reads excluded.')
+
+    parser_single.add_argument('-o', '--output-folder', required=True,
+                        help='Specify the output folder to create. If already created, it must be empty.')
+
+    parser_single.add_argument('-is', '--insertion-fasta', required=True, type=str,
                         default=os.path.join(data_dir, "IS_fastas/Bacteroides_all.fasta"),
                         help='A fasta file containing the insertion sequences of interest,'
                              ' concatenated sequentially in any order.')
 
-    parser.add_argument('-o', '--output_folder', required=False,
-                        default = os.abspath(os.path.join(output_dir,"ISMapper_%s" % timestamp)),
-                        help='Specify the output file')
+    genomes_in = parser_single.add_mutually_exclusive_group(required=True)
+    genomes_in.add_argument('-gf', '--reference-genome-fastas',
+                             type=genome_fasta, nargs='*',
+                             help='All the reference genomes to analyze in fasta format. Input must be '
+                                'in the format \"<reference-fasta1>,Taxon1 <reference-fasta2>,Taxon2\"')
+    genomes_in.add_argument('-gl', '--reference-genome-list',
+                             type=genome_list, nargs=1,
+                             help='A path to a tab-seperated file that contains the Taxon in the first '
+                                  'column, and the path to the corresponding genome fasta in the second column.')
 
-    parser.add_argument('-p', '--threads', required=False,
+    parser_single.add_argument('-nodes', '--taxon-nodes', required=False, type=list,
+                               default=[
+                                   os.path.abspath(os.path.join(data_dir, "TaxonomyDatabase/nodes.dmp")),
+                                   os.path.abspath(os.path.join(data_dir, "TaxonomyDatabase/merged.dmp"))],
+                               help='Location of the NCBI Taxonomy Database nodes.dmp and merged.dmp files',
+                               nargs='*')
+
+    parser_single.add_argument('-p', '--threads', required=False,
                         default=1, type = int,
-                        help='The number of threads to run with bowtie2.')
+                        help='The number of threads to run with bowtie2 alignments.')
 
     args = parser.parse_args()
     args = vars(args)
 
-    main(args)
-
-
+    pprint(args)

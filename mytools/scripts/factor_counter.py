4e1b8c9233a0a35b0bcea06eb2245d2e882e6543
M Durrant <mdurrant@DN52e6fo.SUNet>
2016-12-05 13:47:44 -0700

    update

--- mytools/scripts/factor_counter.py
+++ mytools/scripts/factor_counter.py
@@ -5,12 +5,17 @@ import argparse
 def main(args):
     colnum = args['column']
     file_in = args['file']
+    delimiter = args['delimiter']
 
     factor_counts = defaultdict(int)
     with open(file_in) as infile:
         for line in infile:
-            line = line.strip().split()
+            line = line.strip().split(delimiter)
             factor_counts[line[colnum]] += 1
+
+    print delimiter.join(['Factor', 'Count'])
+    for key in factor_counts:
+        print delimiter.join([key, factor_counts[key]])
 
 
 if __name__ == "__main__":
@@ -19,6 +24,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Quickly get the taxon id for a given')
     parser.add_argument('file', type = str)
     parser.add_argument('-c', '--column', required=True, type=int,
+                        help='FILL THIS OUT')
+    parser.add_argument('-d', '--delimiter', required=False, default='\t',
                         help='FILL THIS OUT')
 
     args = parser.parse_args()
